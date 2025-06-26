34ef7fc23094be7ed01a79f7396097a1e807bf48
atulyado <atulyado198@gmail.com>
2025-06-26 11:15:09 +0700

    make the sentence reverse

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Reverse-word
 LEETCODE QUESTION
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "Mr Ding"
+Output: "rM gniD"
---
+++ reverseWord.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        
+        for(String word : words){
+            StringBuilder reversed = new StringBuilder(word);
+            result.append(reversed.reverse().toString()).append(" ");
+        }
+        return result.toString().trim(); 
+    }
+}
