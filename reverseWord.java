0dfe2c00cc0d56b5d3aae4137d5e75627191f028
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-27 20:47:30 +0700

    Update reverseWord.java

--- reverseWord.java
+++ reverseWord.java
@@ -1,12 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split(" ");
+        String[] words = s.split(" ");//This split the string by the space
         StringBuilder result = new StringBuilder();
         
         for(String word : words){
             StringBuilder reversed = new StringBuilder(word);
-            result.append(reversed.reverse().toString()).append(" ");
+            result.append(reversed.reverse().toString()).append(" ");//here we reverse the the sentece and change it back to the normal string and add space
         }
-        return result.toString().trim(); 
+        return result.toString().trim(); //here change the sentence back to string remove the space at the end
     }
 }
